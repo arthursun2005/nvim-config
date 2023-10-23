bac99d7a29fc994f06d872b4d337afb8a97ba072
arthur <arthur@snorlax>
2023-10-23 11:37:22 +1100

    no spin

--- lua/astre/remap.lua
+++ lua/astre/remap.lua
@@ -11,9 +11,9 @@ vim.keymap.set("n", "<c-f>", ":RG<cr>")
 vim.keymap.set("n", "<leader>w", ":UndotreeToggle<cr>")
 
 vim.api.nvim_create_autocmd("FileType", {
-        pattern = {'cpp', 'cuda'},
+        pattern = {'cpp'},
         callback = function(args)
-                vim.keymap.set("n", "<leader>c", ":up<cr>:!eval $(spin cc \"%\" bin/\"%:r\")<cr>", { buffer = args.buf })
+                vim.keymap.set("n", "<leader>c", ":up<cr>:!g++ -O2 -std=gnu++20 -DDEBUG -Wall -Wextra -Wshadow\"%\" -o \"%:r\"<cr>", { buffer = args.buf })
         end
 })
 
